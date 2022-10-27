553bf880cf2412121f681843f3d738eaaa85b9ff
nnuoyos <youngadult3104@gmail.com>
2022-10-27 23:26:01 +0900

    db url

--- src/page/ProductAll.js
+++ src/page/ProductAll.js
@@ -9,8 +9,8 @@ const ProductAll = () => {
   const getProducts = async () => {
     let searchQuery = query.get('q') || '';
     console.log('쿼리 값은?', searchQuery);
-    let url = `https://my-json-server.typicode.com/nnuoyos/hnm_react/products?=?q=${searchQuery}`
     /* let url = `http://localhost:3004/products?q=${searchQuery}` */
+    let url = `https://my-json-server.typicode.com/nnuoyos/hnm_react/products?=?q=${searchQuery}`
     let response = await fetch(url)
     let data = await response.json()
     setProductList(data);
