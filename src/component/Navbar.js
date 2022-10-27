acca0ccf20ef263d58b969e31f40bb708b97224f
nnuoyos <youngadult3104@gmail.com>
2022-10-27 17:36:03 +0900

    넷리파이 업로드 테스트

---
+++ src/component/Navbar.js
@@ -0,0 +1,50 @@
+import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUser } from '@fortawesome/free-regular-svg-icons';
+import {faSearch} from '@fortawesome/free-solid-svg-icons';
+import { useNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
+
+const Navbar = ({authenticate, setAuthenticate}) => {
+/* 코드의 재생산, 유지보수를 위해 메뉴의 배열을 만들어준다 */
+const menuList = ['여성','Divided','남성','신생아/유아','아동','H&M HOME','Sale','지속가능성']
+const navigate = useNavigate()
+const search = (event) => {
+    if (event.key === "Enter"){
+        let keyword = event.target.value
+        navigate(`/?q=${keyword}`)
+        console.log("on key press 확인",keyword)
+    }
+}
+
+  return (
+    <div>
+        <div className='login-button'>
+            {/* true 라면 로그인이 되어 있는상태이므로 상품페이지를 보여주고 로그아웃 글자로 바꿔줌*/}
+            {authenticate === true ? (<div className='login-inner' onClick={()=> setAuthenticate(false)}>
+                <FontAwesomeIcon icon={faUser} />
+                <div className='login-text'>로그아웃</div>
+            </div>) : (<div className='login-inner' onClick={()=>navigate('/login')}>
+                <FontAwesomeIcon icon={faUser} />
+                <div className='login-text'>로그인</div>
+            </div>)}
+        </div>
+        <div className='nav-section'>
+            <Link to="/">
+            <img src='https://logos-world.net/wp-content/uploads/2020/04/HM-Logo.png' alt='main page logo'></img>
+            </Link>
+        </div>
+        <div className='menu-area'>
+            <ul className='menu-list'>
+                {menuList.map((menu)=>(<li>{menu}</li>))}
+            </ul>
+            <div className='nav-search'>
+                <FontAwesomeIcon icon={faSearch} />
+                <input onKeyPress={(event)=>search(event)} placeholder='제품검색'></input>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
---
+++ src/page/ProductDetail.js
@@ -0,0 +1,44 @@
+import React, {useEffect, useState} from 'react'
+import { useParams } from 'react-router-dom'
+import { Container, Row, Col } from 'react-bootstrap';
+import Dropdown from 'react-bootstrap/Dropdown';
+
+const ProductDetail = () => {
+  let {id} = useParams();
+  const [product, setProduct] = useState(null);
+  const getProductDetail = async() => {
+   /*  let url = (`https://my-json-server.typicode.com/nnuoyos/front-hnm${id}`) */
+    let url = (`http://localhost:3004/products/${id}`)
+    let response = await fetch(url) //url을 fetch 해준다
+    let data = await response.json()
+    //console.log("데이터 잘 들어왔는지?" ,data);
+    setProduct(data);
+  }
+  useEffect(()=>{
+    getProductDetail()
+  },[])
+  return (
+    <Container>
+    <Row>
+      <Col className='product-img'><img src={product?.img}></img></Col>
+      <Col>
+        <div>{product?.title}</div>
+        <div>₩{product?.price}</div>
+        <Dropdown>
+          <Dropdown.Toggle variant="success" id="dropdown-basic">
+            size choice
+          </Dropdown.Toggle>
+          <Dropdown.Menu>
+            {/* product 가 true 라면 실행되어야 한다 그렇지 않으면 에러 발생 */}
+            {product?.size.length > 0 && product.size.map((item)=>(<Dropdown.Item href="#/action-1">{item}</Dropdown.Item>))}
+          </Dropdown.Menu>
+        </Dropdown>
+        <div className='card-conscious-choice'>{product?.choice === true ? "Conscious choice" : <br></br>}</div>
+        <div className='card-new-item'>{product?.new === true ? "신제품" : <br></br>}</div>
+      </Col>
+    </Row>
+  </Container>
+  )
+}
+
+export default ProductDetail
